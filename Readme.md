75cd670abf47f8485c5959817966d98d132ce2ae
Dimitris Georgiadis <zibraogi@gmail.com>
2021-01-10 00:25:50 +0200

    version 1.0

---
+++ Readme.md
@@ -0,0 +1,33 @@
+### to run it do
+
+
+### in MySQL Workbench
+
+```SQL
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    am INT NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+INSERT INTO users(name, email, am)VALUES('anna','an2@uowm.gr',5849);
+
+INSERT INTO users(name, email, am)VALUES('kostas','kos7@uowm.gr',7695);
+
+INSERT INTO use(name, email, am)VALUES('alex','alw5@uowm.gr',2278);
+```
+
+### in terminal
+
+```npm install```
+
+
+```nodemon```
+
+
+### go to http://localhost:3000/
+
+
+![GitHub Logo](sample.jpg)
+Format: ![Alt Text](url)
---
+++ server.js
@@ -0,0 +1,110 @@
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "admin2",
+  password: "1qa2ws",
+  database: "uowm",
+  port: 3307,
+});
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("MySQL Connected successfully..");
+  }
+});
+
+//set views file
+app.set("views", path.join(__dirname, "views"));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Display all Data
+app.get("/", (req, res) => {
+  let sql = "SELECT * FROM users ORDER by name ASC";
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    res.render("user_index", {
+      title: "CRUD App using NodeJS / ExpressJS / MySQL",
+      users: rows,
+    });
+  });
+});
+
+//Add Request
+app.get("/add", (req, res) => {
+  res.render("user_add", {
+    title: "Add New User",
+  });
+});
+
+//Save the add item
+app.post("/save", (req, res) => {
+  let data = {
+    name: req.body.name,
+    email: req.body.email,
+    am: req.body.am,
+  };
+  let sql = "INSERT INTO users SET ?";
+  db.query(sql, [data], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.redirect("/");
+  });
+});
+
+app.get("/edit/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let sql = "Select * from users where id = ?";
+  db.query(sql, [userId], (err, result) => {
+    if (err) throw err;
+    res.render("user_edit", {
+      title: "Edit user",
+      user: result[0],
+    });
+  });
+});
+
+// Update Request
+app.post("/update", (req, res) => {
+  const userId = req.body.id;
+  const name = req.body.name;
+  const email = req.body.email;
+  const am = req.body.am;
+
+  let sql = "update users SET name= ?, email= ? , am= ? where id = ?";
+  db.query(sql, [name, email, am, userId], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.redirect("/");
+  });
+});
+
+//Delete Request
+app.get("/delete/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let sql = "DELETE from users where id = ?";
+  db.query(sql, userId, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+// Server Listening
+app.listen(3000, () => {
+  console.log("server started at port 3000");
+  console.log("click here " + " http://localhost:3000/");
+});

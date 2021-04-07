7683c7210c0342e38e966650d99e96a55df58021
Jimgeo98 <geojimas@gmail.com>
2021-04-08 00:39:40 +0300

    sql fix

--- db/res.sql
+++ db/res.sql
@@ -7,22 +7,22 @@ USE people;
 
 CREATE TABLE users (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-pname VARCHAR(30) NOT NULL,
+name VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
 am INT NOT NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
---Tests
+-- Tests
 -- DROP TABLE users;
 
-INSERT INTO users(pname, email, am)
+INSERT INTO users(name, email, am)
 VALUES('Lisa','lis@gmail.com',22);
 
-INSERT INTO users(pname, email, am)
+INSERT INTO users(name, email, am)
 VALUES('John','john@gmail.com',28);
 
-INSERT INTO users(pname, email, am)
+INSERT INTO users(name, email, am)
 VALUES('Alex','ale@yahoo.com',19);
 
 
@@ -47,83 +47,3 @@ VALUES('Alex','ale@yahoo.com',19);
 
 
 
-
-
--- logs table
-
-CREATE TABLE loggs (
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(30) NOT NULL,
-actionKind VARCHAR(30) NOT NULL,
-actionTime DATETIME NOT NULL,
-tableName VARCHAR(30) NOT NULL,
-oldName VARCHAR(30),
-oldemail VARCHAR(30),
-oldAm INT,
-newName VARCHAR(30),
-newEmail VARCHAR(30),
-newAm INT
-);
-
--- DROP TABLE loggs;
-
-
-
-
--- trigger for insert
-delimiter |
-
-CREATE TRIGGER insertTrigger AFTER INSERT ON users FOR EACH ROW
-  BEGIN
-	INSERT INTO loggs(username,actionKind,actionTime,tableName,newName,newEmail,newAge)
-    VALUES(current_user(),'INSERT',current_time(),'STUDENTS',new.pname,new.email,new.am);
-  END;
-|
-
-delimiter ;
-
-
-
--- trigger for update
-delimiter |
-
-CREATE TRIGGER updateTrigger AFTER UPDATE ON users FOR EACH ROW
-  BEGIN
-	IF OLD.pname <> NEW.pname OR OLD.email <> NEW.email THEN
-		INSERT INTO loggs(username,actionKind,actionTime,tableName,oldName,oldemail,oldAge,newName,newEmail,newAge)
-		VALUES(current_user(),'UPDATE',current_time(),'STUDENTS',old.pname,old.email,old.age,new.pname,new.email,new.am);
-        UNLOCK TABLES;
-	END IF;
-  END
-|
-
-delimiter ;
-
-
-
--- trigger for delete
-
-delimiter |
-
-CREATE TRIGGER deleteTrigger AFTER DELETE ON users FOR EACH ROW
-  BEGIN
-	INSERT INTO loggs(username,actionKind,actionTime,tableName,oldName)
-	VALUES(current_user(),'DELETE',current_time(),'STUDENTS',old.pname);
-    UNLOCK TABLES;
-  END
-|
-
-delimiter ;
-
-
---Tests
-
--- SET SQL_SAFE_UPDATES = 0;
--- DELETE FROM loggs;
--- DELETE FROM users;
-
-
--- DROP TRIGGER insertTrigger;
--- DROP TRIGGER updateTrigger;
-
---Tests
